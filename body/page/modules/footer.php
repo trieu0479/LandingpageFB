929db55d68fc8859106d10dcd4a46049433ceac5
trieu0479@gmail.com <trieu0479@gmail.com>
2020-04-28 11:13:18 +0700

    up blue 28/4

--- body/page/modules/footer.php
+++ body/page/modules/footer.php
@@ -1,11 +1,20 @@
     <div id="fb-root"></div>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=<?=$appId?>&autoLogAppEvents=1"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="<?=$rootURL?>/dist/js/extension/datatables.min.js"></script>
     <script src="<?=$rootURL?>/dist/js/extension/dataTables.buttons.min.js"></script>
 
     <script src="<?=$rootURL?>/dist/js/bootstrap.min.js"></script>
     <script src="<?=$rootURL?>/dist/js/extension/sweetalert2.min.js"></script>
-    <script src="<?=$rootURL?>/dist/js/extension/moment.min.js"></script>
+    <!-- <script src="<?=$rootURL?>/dist/js/extension/moment.js"></script> -->
+    <script src="<?=$rootURL?>/dist/js/flatpickr.js"></script>
+    <script src="<?=$rootURL?>/dist/js/flatpickr_vn.js"></script>
+    <script src="<?=$rootURL?>/dist/js/echarts.min.js"></script>
+    <script src="<?=$rootURL?>/dist/js/moment.js"></script>
+    <script src="<?=$rootURL?>/dist/js/numeral.min.js"></script>
+    <script src="<?=$rootURL?>/dist/js/ResizeSensor.js"></script>
+    
+    
   </body>
 </html>
--- body/page/modules/header.php
+++ body/page/modules/header.php
@@ -49,7 +49,9 @@
     <link href="<?=$rootURL?>/dist/css/pages/stepper.css" rel="stylesheet">
     
     <link href="<?=$rootURL?>/dist/css/fff/reset.css?v=<?=$version?>" rel="stylesheet">
-    
+    <link rel="stylesheet" href="dist/css/flatpickr.min.css">
+    <link rel="stylesheet" href="dist/css/chartthem.css">
+  
     <script src="https://apis.google.com/js/api:client.js"></script>
     <script src="<?=$rootURL?>/dist/js/extension/jquery.min.js"></script>
     <script src="<?=$rootURL?>/dist/js/default/controlGlobalValule.js"></script>
