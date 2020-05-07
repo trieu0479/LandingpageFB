a1e2f40ab5b48a4ee72f0899818537f0ec452e82
longlong31 <hiennpps06647@fpt.edu.vn>
2020-05-07 14:52:50 +0700

    up

--- body/body/stats/index.php
+++ body/body/stats/index.php
@@ -1,7 +1,24 @@
+<link rel="stylesheet" href="dist/css/pages/stats.css">
 <div class="container">
-    <div class="row">
+    <div class="row my-5">
         <div class="col">
-            <h1>dajsdjkasdalksdjaskjd</h1>
+            <div class="font-gg font-weight-500 fontsize-16 text-success text-center">
+                Top Fanpage
+            </div>
+            <div class="font-gg font-weight-400 fontsize-32 text-center">
+                Top Facebook Fanpage Việt Nam 
+            </div>
+            <div class="font-gg text-muted font-14 text-center mt-2 font-weight-400 mx-auto" style="max-width: 700px;">
+                Nhập vào tên miền (domain) website cần phân tích, công cụ phân tích traffic sẽ cung cấp cho bạn mọi thông tin về website 
+            </div>
+            <div class="input-group mt-4 w-100 mx-auto" style="max-width: 700px;">
+                <div class="input-group keywordSearchBox">
+                    <input type="text" class="form-control iptWebsite py-3 rounded mr-2" aria-label="Nhập tên miền website của bạn vào đây" value="" placeholder="Nhập tên miền website của bạn vào đây">
+                    <div class="input-group-append">
+                        <button class="btnCheckWebsite btn btn-success font-gg font-weight-500 rounded shadow-sm px-5" type="button"><i class="fad fa-bolt mr-2 font-13"></i>Phân Tích</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
