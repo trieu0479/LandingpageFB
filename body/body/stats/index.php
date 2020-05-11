2bae809b08f0c645efcbe27af5f6f098c7d63bef
DESKTOP-RPAIILR\HuuHai <ndtmivn123@gmail.com>
2020-05-11 16:36:04 +0700

    commit

--- body/body/stats/index.php
+++ body/body/stats/index.php
@@ -1,33 +1,35 @@
 <link rel="stylesheet" href="dist/css/pages/stats.css">
-<!-- <div class="container"> -->
+<div class="container">
     <div class="row my-5">
         <div class="col">
             <div class="text-center pb-5 pt-5 pt-md-5">
             <div class="fontSize-16 font-weight-bold text-success">FFF BLUE</div>
             <div class="fontSize-24 font-weight-bold text-primary">Công Cụ Tăng Tương Tác Facebook</div>
-
             <div class="text-center mt-2 mb-2 extensionLogo">
                 <a target="blank" href="https://chrome.google.com/webstore/detail/facebook-tools/kccoacihbkcjdkjmhpaojhonlcjnmmld"><img src="https://webrank.vn/dist/images/chrome-extension.png" class="mr-2"></a>
                 <a target="blank" href="https://microsoftedge.microsoft.com/addons/detail/fhohbkpkplmojpkmgbcejdikljogdolh"><img src="https://webrank.vn/dist/images/edge-extension.png"></a>
             </div>
-            <div class="fontSize-14 text-muted mb-2">Tạo khóa link, yêu cầu người dùng tương tác (Like và Comment) vào post trên Facebook để mở khóa </div>        
+            <div class="fontSize-14 text-muted mb-2">Tạo khóa link, yêu cầu người dùng tương tác (Like và Comment) vào post trên Facebook để mở khóa </div>     
         </div>
-            <div class="maxWidth-800 pb-5 m-auto mrl-md-1">
-                <div class="input-group mt-3">
-                    <input type="text" class="form-control fontSize-14 p-2 ml-1 font-12 rounded mr-2" aria-label="Nhập tên miền website của bạn vào đây" value="" placeholder="Nhập tên miền website của bạn vào đây">
-                    
-                    <div class="input-group-append">
-                        <button class="ml-auto  btn-Step1 btn btn-info"><i class="fad fa-layer-plus mr-2"></i> Tiếp tục</button>
-                    </div>
+        <div class="maxWidth-800 pb-5 m-auto mrl-md-1">
+            <div class="input-group mt-3">
+                <input type="text" class="form-control fontSize-14 p-2 ml-1 font-12 rounded mr-2" aria-label="Nhập tên miền website của bạn vào đây" value="" placeholder="Nhập tên miền website của bạn vào đây">
+                <div class="input-group-append">
+                    <button class="ml-auto  btn-Step1 btn btn-info"><i class="fad fa-layer-plus mr-2"></i> Tiếp tục</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script>
-    $.get(`//v7-fffblue.com/server/stats.php?task=getFacebookInformation&userToken=${userToken}&fbId=101162088206729`,function(data){
-        // data = JSON.parse(data)
-        
-        console.log(data)
-    })
-</script>
+
+    <div class="row">
+        <div class="col-12 col-12 pb-3 pb-lg-5">
+            <div class="content-table mt-5 text-left m-auto" style="white-space:nowrap">
+                <table class="table table-qc-format mb-0 dataTable no-footer table-fbRank" id="tablefbRank">
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+<script src="dist/js/pages/stats.js"></script>
---
+++ dist/js/pages/stats.js
@@ -0,0 +1,142 @@
+const language = {
+    searchPlaceholder: 'Nhập từ khóa',
+    processing: 'Đang xử lý...',
+    loadingRecords: 'Đang tải...',
+    emptyTable: 'Không có dữ liệu hiển thị',
+    lengthMenu: 'Hiển thị: _MENU_ dòng mỗi trang',
+    zeroRecords: 'Không tìm thấy dữ liệu',
+    info: 'Hiển thị từ _START_ đến _END_ trong tổng _TOTAL_ dòng',
+    infoEmpty: 'Hiển thị từ 0 đến 0 trong tổng 0 dòng',
+    infoFiltered: '(lọc từ tổng số _MAX_ dòng)',
+    search: 'Tìm kiếm:',
+    paginate: {
+        previous: '<i class="fa fa-angle-left kt-font-brand"></i>',
+        next: '<i class="fa fa-angle-right kt-font-brand"></i>'
+    }
+};
+
+
+function renderData(data) {
+    // var
+    var arrtTable = [];
+    let stt = 0
+
+    // console.log('dmm', data)
+    $.each(data.data, function(k, v) {
+        stt++
+        var output = {};
+        output.stt = stt
+        output.fbId = v.fbId;
+        output.fanpageCover = v.fanpageCover
+        output.fanpageName = v.fanpageName
+        output.fbCategory = v.fbCategory
+        output.pageAlias = v.pageAlias
+
+        output.hieu = v.likes - v.likes_yesterday
+
+
+        if (hieu > 0) {
+            $(".take-care-like").append('<i class="fad fa-arrow-alt-square-up"></i>')
+        } else if (hieu < 0) {
+            $(".take-care-like").append('<i class="fad fa-arrow-alt-square-down"></i>')
+        }
+
+        output.likes = v.likes
+        output.likes_yesterday = v.likes_yesterday
+
+        // console.log(hieu)
+
+
+
+        if (!v.website) {
+            output.website = "chưa có"
+        } else {
+            output.website = v.website
+        }
+        arrtTable.push(output);
+    })
+    return arrtTable;
+}
+
+
+
+
+function getData() {
+    $.ajax({
+        url: `//v7-fffblue.com/server/stats.php?task=getAllFacebookInformation&userToken=Vm5ZSmVLTjhXcWYwRzFObXlnbk5WUmlIdXF0Zk5XaGpkbXJ5ODMwc3J6Yz06OnD33aPxFDTCO6LhohyjG8o&limit=20`,
+        type: "GET"
+    }).then(data => {
+        data = JSON.parse(data)
+        if (data.length == 0) {
+            $('#dataList').addClass('d-none')
+                // console.log(2)
+        } else {
+            // console.log(1)
+            $(`#tablefbRank`).DataTable({
+                data: renderData(data),
+                // drawCallback: function(settings) {},
+                columns: [{
+                        title: `<div class="text-capitalize font-weight-bold font-12 text-center m-auto" style="max-width:30px;width:30px">Stt</div>`,
+                        "data": data => `<div class="text-center m-auto">${data.stt}</div>`
+                    }, {
+                        title: `<div class="text-capitalize font-weight-bold font-12 text-center m-auto" style="max-width:200px;width: 200px">Tên FanPage</div>`,
+                        "data": data => `<div class="text-left mr-auto text-cut" ><a href="https://facebook.com/${data.fbId}"> <img src="${data.fanpageCover}" class="img-fluid rounded-circle" style="object-fit:cover; height:40px; width:40px"><span href="https://facebookcom/${data.fbId}" class="text-primary pl-3 text-left mr-auto cut-text-title">${data.fanpageName}</span></a></div>`,
+                    },
+                    {
+                        title: `<div class="text-capitalize font-weight-bold font-12 text-center m-auto">Danh Mục</div>`,
+                        "data": data => `<div class="text-dark text-left mr-auto cut-text-category" style="margin-top: 12px;"> <span>${data.fbCategory }</span></div>`,
+                    },
+                    {
+                        title: `<div class="text-capitalize font-weight-bold font-12 text-center m-auto">Website</div>`,
+                        "data": data => `
+                        <div class="text-center m-auto" style="margin-top: 12px;">
+                            <a href="${data.website}">
+                                <span class="btn btn-result-kq btn-default btn-sm rounded-pill px-3 py-2 font-13"><i class="fad fa-eye text-info mr-2 font-14 font-weight-bold"></i>Đi đến</span>
+                            </a>
+                        </div>`,
+                    },
+                    {
+                        title: `<div class="text-capitalize font-weight-bold font-12 text-center m-auto" style="max-width:100px;width:100px">Lượt thích</div>`,
+                        "data": data => `
+                        <div class="text-center" style="margin-top: 12px;" style="max-width:100px;width:100px">
+                            <div class="take-care-likes">
+                                <span class="like-today">${data.likes}</span> vs <span class="like-today">${data.likes_yesterday}</span>
+                               
+                            </div>
+                        </div>
+                        `
+                    }
+                ],
+
+
+
+
+                initComplete: function(settings, json) {
+                    // $(`#table-manager td`).attr('style', 'padding:10px 18px')
+                    $(`#table-manager_wrapper .dataTables_scrollBody`).perfectScrollbar();
+                    $(`.tabletable-manager`).removeClass('is-loading')
+                        // $(`.table th, .table thead`).addClass('thead-light')
+                        // console.log(data.hieu)
+
+
+                },
+                destroy: true,
+                rowId: 'trId',
+                "dom": 'ftp',
+                paging: false,
+                // scrollY: '570px', 
+                scrollX: false,
+                "ordering": false,
+                info: false,
+                autoWidth: false,
+                processing: true,
+                searching: false,
+                language
+            })
+        }
+
+    })
+}
+
+
+getData();
