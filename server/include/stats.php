10f64bd0504f1dd648acb761319773005b77388a
longlong31 <hiennpps06647@fpt.edu.vn>
2020-05-11 08:44:03 +0700

    up

--- server/include/stats.php
+++ server/include/stats.php
@@ -5,7 +5,7 @@ class stats{
     var $cookie = null;
     public function __construct(){
         $mysqli = new mysqli ('210.211.116.252', '2020_u_fffblue', '2020_u_fffblue', '2020_fff_facebook');
-        $this->db =new MysqliDb ($mysqli);
+        $this->db = new MysqliDb ($mysqli);
     }
     function checkUserLevel($userToken){
         return $this->getUserId($userToken);
@@ -22,9 +22,23 @@ class stats{
         }else return null;
     }
     function getFacebookInformation($fbId){
-        echo $fbId;
+        $this->db->where("fbId",$fbId);
+        $informationFb = $this->db->getone('facebook_fanpage');
+        // echo $this->db->getLastQuery();
+        $data = [];
+        if($informationFb) {
+            $data = $informationFb;
+        //     foreach($informationFb as $key => $value) {
+        //         $data["fbId"] = $value["fbId"];
+        //         $data["fbCategory"] = $value["fbCategory"];
+        //         $data["fanpageName"] = $value["fanpageName"];
+        //     }
+        }
+        return $data;
     }
-
+    function getFacebookLikeDay($fbId){
+        $this->db->where("fbId",$fbId);
+        $this->db->orWhere ('firstName', 'Peter');
+    }
 }
-
 ?>
