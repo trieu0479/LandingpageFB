21911d386ec7a0343b2f4f2a10f0d1b31bdda355
DESKTOP-RPAIILR\HuuHai <ndtmivn123@gmail.com>
2020-05-14 08:51:39 +0700

    comit

--- body/body/stats/detail.php
+++ body/body/stats/detail.php
@@ -9,53 +9,134 @@
 				<img src="../../dist/images/facebook-cover/top-header1.jpg" alt="" class="img-facebookCover">
 			</div>
 			<div class="profile-section">
+				
+			<div class="pt-3 pb-3 d-flex justify-content-between bg-white w-100">
+				<div class="flatpickr ml-md-3 h-100 d-flex no-block p-2 justify-content-end align-items-center" style="border: 1px solid #edf1f5; background: #edf1f5; border-radius:5px">
+					<input type="text" style="width:170px; border: none; background: #edf1f5;"  id="rangeDateSeo" placeholder="Please select Date Range" data-input="" class="flatpickr-input" readonly="readonly">
+					<a class="input-button" title="toggle" data-toggle=""><i class="fal fa-calendar-alt font-14 ml-3"></i></a>
+				</div>
+			</div>
 
+				<div class="control-block-button">
+					<a href="" target="_blank" class="btn btn-control bg-blue" id="fbLink">
+						<i class="fab fontsize-20 fa-facebook-f"></i>
+					</a>
+
+					<a href="" target="_blank" class="btn btn-control bg-purple" id="webLink">
+						<i class="fad fontsize-20 fa-browser"></i>
+					</a>
+					
+					<div class="btn btn-control bg-oranger" id="phoneNumb" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+						<i class="fad fontsize-20 fa-mobile-alt"></i>
+					</div>
+				</div>
 			</div>
 			<div class="top-header-author">
 				<div class="author-thumb">
-					<img id="fbCover" src="../../dist/images/facebook-cover/author-main1.jpg" alt="author">
+					<img id="fbCover" src="" alt="author">
 				</div>
 				<div class="author-content">
-					<div class="h4 author-name" id="fbName"></div>
-					<div class="country" id="fbCate"></div>
+					<div class="similarDomain dashed font-weight-500 text-center justify-content-center fontsize-18 font-weight-bold text-primary d-flex" id="fbName"></div>
+					<div class="similarDomain dashed font-weight-500 text-center justify-content-center fontsize-16 font-weight-bold text-dark d-flex " id="fbCate"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+
+
+		<div class="col-lg-3 col-md-6 col-12 mt-3 px-0" style="padding-left:15px !important">
+			<div class="box-info bg-white p-4 border-right">
+				<p class="note-top fontsize-14 text-center">
+					top grade
+				</p>
+				<h3 class="text-first text-info fontsize-44 text-center">
+					A++
+				</h3>
+				
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-6 col-12 mt-3 px-0">
+			<div class="box-info bg-white p-4 border-right">
+				<p class="note-top fontsize-14 text-center">
+					LIKES RANK
+				</p>
+				<h3 class="text-first text-success fontsize-44 text-center">
+					10th
+				</h3>
+				
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-6 col-12 mt-3 px-0">
+			<div class="box-info bg-white p-4 border-right">
+				<p class="note-top fontsize-14 text-center">
+					TALKING ABOUT RANK
+				</p>
+				<h3 class="text-first text-danger fontsize-44 text-center">
+				8,786th
+				</h3>
+				
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-6 col-12 mt-3 px-0" style="padding-right:15px !important">
+			<div class="box-info bg-white p-4 border-right">
+				<p class="note-top fontsize-14 text-center">
+					CATEGORY RANK
+				</p>
+				<h3 class="text-first text-secondary fontsize-44 text-center">
+					A++
+				</h3>
+				
+			</div>
+		</div>
+
+
+	</div>
+</div>
+
+
+<!-- <div class="container mt-3">
+	<div class="row">
+		<div class=" col-md-12 d-flex">
+			<div class="pt-3 pb-3 d-flex justify-content-between bg-white w-100">
+				<div class="flatpickr ml-md-3 h-100 d-flex no-block p-2 justify-content-end align-items-center" style="border: 1px solid #edf1f5; background: #edf1f5; border-radius:5px">
+					<input type="text" style="width:170px; border: none; background: #edf1f5;"  id="rangeDateSeo" placeholder="Please select Date Range" data-input="" class="flatpickr-input" readonly="readonly">
+					<a class="input-button" title="toggle" data-toggle=""><i class="fal fa-calendar-alt font-14 ml-3"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> -->
+
+
+<div class="container mb-4">
+	<div class="row">
+		<div class="col-lg-12 col-md-6 col-12 mt-3">
+			<div class="ui-block bg-white">
+				<div class="ui-block-title">
+					<h6 class="title">Biểu đồ thống kê lượt like</h6>
+				</div>
+				<div class="box-chart-likes bg-white" id="chartLikes" style="height:315px">
+
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
+
+
 <div class="container mb-4">
 	<div class="row">
-		<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-			<div class="ui-block mt-3 bg-white" style="border: 1px solid #e6ecf5">
-				<div class="ui-block-title">
-					<h6 class="title">Profile Intro</h6>
-				</div>
-				<div class="ui-block-content">
-					
-					<ul class="widget w-personal-info item-block pl-4 pr-4">
-						<li>
-							<span class="title">About Me:</span>
-							<span class="text">Hi, I’m James, I’m 36 and I work as a Digital Designer for the  “Daydreams” Agency in Pier 56.</span>
-						</li>
-						<li>
-							<span class="title">Favourite TV Shows:</span>
-							<span class="text">Breaking Good, RedDevil, People of Interest, The Running Dead, Found,  American Guy.</span>
-						</li>
-						<li>
-							<span class="title">Favourite Music Bands / Artists:</span>
-							<span class="text">Iron Maid, DC/AC, Megablow, The Ill, Kung Fighters, System of a Revenge.</span>
-						</li>
-					</ul>
-				</div>
+		<div class="col-12 col-12 pb-3 pb-lg-5">
+			<div class=" ui-block-title text-left py-3 bg-white">
+				<h6 class="title">Bảng thống kê lượt like</h6>
 			</div>
-		</div>
-
-		<div class=" col-lg-9 col-md-6 col-sm-6 col-12 mt-3 bg-white">
-			<div class="ui-block ">
-				<div class="box-chart-likes bg-white" id="chartLikes" style="height:300px">
-
-				</div>
+			<div class="content-table mt-5 text-left m-auto">
+				<table class="table mb-0 dataTable borderless no-footer" id="tableRank-fb">
+				</table>
 			</div>
 		</div>
 	</div>
--- dist/js/pages/detail.js
+++ dist/js/pages/detail.js
@@ -1,7 +1,60 @@
 var url_ = new URL(location.href);
 var fbId = url_.searchParams.get("fbId")
+var from = url_.searchParams.get("start")
+var to = url_.searchParams.get("end")
+
+const language = {
+    searchPlaceholder: 'Nhập từ khóa',
+    processing: 'Đang xử lý...',
+    loadingRecords: 'Đang tải...',
+    emptyTable: 'Không có dữ liệu hiển thị',
+    lengthMenu: 'Hiển thị: _MENU_ dòng mỗi trang',
+    zeroRecords: 'Không tìm thấy dữ liệu',
+    info: 'Hiển thị từ _START_ đến _END_ trong tổng _TOTAL_ dòng',
+    infoEmpty: 'Hiển thị từ 0 đến 0 trong tổng 0 dòng',
+    infoFiltered: '(lọc từ tổng số _MAX_ dòng)',
+    search: 'Tìm kiếm:',
+    paginate: {
+        previous: '<i class="fa fa-angle-left kt-font-brand"></i>',
+        next: '<i class="fa fa-angle-right kt-font-brand"></i>'
+    }
+};
+
+if (!fbId) {
+    window.location.href = 'http://v7-fffblue.com/stats.php/?view=stats&action=index';
+    console.log(1)
+} else {
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    function getDataFB() {
+        $.ajax({
+            url: `//v7-fffblue.com/server/stats.php?task=getFacebookInformation&userToken=Vm5ZSmVLTjhXcWYwRzFObXlnbk5WUmlIdXF0Zk5XaGpkbXJ5ODMwc3J6Yz06OnD33aPxFDTCO6LhohyjG8o&fbId=${fbId}`,
+            type: "GET"
+        }).then(res => {
+            res = JSON.parse(res)
+            let fbCover = res.data.fanpageCover
+            let fbName = res.data.fanpageName
+            let fbCate = res.data.fbCategory
+            let fbId = res.data.fbId
+            let fbwebsite = res.data.website
+            let fbphone = res.data.phone
 
 
+
+
+            $('#fbCover').attr("src", fbCover)
+            $('#fbName').text(fbName)
+            $('#fbCate').text(fbCate)
+
+
+            $('#fbLink').attr('href', `http://facebook.com/${fbId}`)
+            fbwebsite ? $('#webLink').attr('href', fbwebsite) : $('#webLink').attr('href', 'javascript:void(0)')
+            fbphone ? $('#phoneNumb').attr('data-original-title', fbphone) : $('#phoneNumb').attr('data-original-title', 'chưa đăng ký')
+        })
+    }
+}
 
 function getDataFB() {
     $.ajax({
@@ -9,45 +62,163 @@ function getDataFB() {
         type: "GET"
     }).then(res => {
         res = JSON.parse(res)
-
+        console.log(res)
         let fbCover = res.data.fanpageCover
         let fbName = res.data.fanpageName
         let fbCate = res.data.fbCategory
+        let fbId = res.data.fbId
+        let fbwebsite = res.data.website
+        let fbphone = res.data.phone
+
+
+
         $('#fbCover').attr("src", fbCover)
         $('#fbName').text(fbName)
         $('#fbCate').text(fbCate)
 
+
+        $('#fbLink').attr('href', `http://facebook.com/${fbId}`)
+        fbwebsite ? $('#webLink').attr('href', fbwebsite) : $('#webLink').attr('href', 'javascript:void(0)')
+        fbwebsite ? $('#webPhone').attr('href', fbwebsite) : $('#webLink').attr('href', 'javascript:void(0)')
     })
+}
+
+
+function renderTable(data) {
+    let bbb = [];
+    // console.log(data.data.length)
+    // data.data.forEach((v, k) => {
+    //         var output = {};
+    //         output.formatDate = moment(v.insertTime, "YYYY/MM/DD").format("DD/MM/YYYY")
+    //         output.likesday = v.likes
+
+    //         columns.push(output)
+    //     })
+    // formatColumns(columns)
+    let columns = data.data
+    let aaa = null
+    if (!columns.length - 1) {
+        aaa = columns.length - 2;
+    } else {
+        aaa = columns.length - 1;
+    }
+    for (i = 0; i < columns.length; i++) {
+        let output = {}
+        output.countlike = columns[aaa].likes - columns[i].likes
+        if (columns[aaa].likes - columns[i].likes > 0) {
+            output.kq = `<span class=" text-success fontsize-14">+${columns[aaa].likes - columns[i].likes}</span><i class="fad text-success bg-success-2 fa-arrow-up ml-2" style="font-size:12px"></i>`
+        } else if (columns[aaa].likes - columns[i].likes < 0) {
+            output.kq = `<span class=" text-danger fontsize-14"> ${columns[aaa].likes - columns[i].likes}</span><i class="fad text-danger fa-arrow-down ml-2" style="font-size:12px"></i>`
+        } else if (columns[aaa].likes - columns[i].likes == 0) {
+            output.kq = `<span class=" text-warning fontsize-14">-</span>`
+        }
+        output.likesday = columns[i].likes
+        output.formatDate = moment(columns[i].insertTime, "YYYY/MM/DD").format("DD/MM/YYYY")
+        bbb.push(output)
+    }
+    return bbb;
 }
 
 
 function getLike10Days() {
+    // let from_ = moment(from).format("YYYY-MM-DD")
+    // let to_ = moment(to).format("YYYY-MM-DD")
+    let from_ = moment(from, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    let to_ = moment(to, 'DD/MM/YYYY').format('YYYY-MM-DD');
+
+    console.log(from)
+    console.log(to_)
+
+
+    $("#rangeDateSeo").flatpickr({
+        locale: "vn",
+        mode: 'range',
+        dateFormat: "d/m/Y",
+        maxDate: new Date(),
+        defaultDate: [from, to],
+        onClose: function(date) {
+            let startDay_ = flatpickr.formatDate(date[0], "d/m/Y");
+            let endDay_ = flatpickr.formatDate(date[1], "d/m/Y");
+            if (startDay_ != from || endDay_ != to) {
+                window.location.href = `?view=stats&action=detail&fbId=${fbId}&start=${startDay_}&end=${endDay_}`
+            }
+
+        }
+    })
+
+
+
+
+
     $.ajax({
-        url: `//v7-fffblue.com/server/stats.php?task=getFacebookLikeDay&userToken=Vm5ZSmVLTjhXcWYwRzFObXlnbk5WUmlIdXF0Zk5XaGpkbXJ5ODMwc3J6Yz06OnD33aPxFDTCO6LhohyjG8o&fbId=${fbId}&from=2020-05-07&to=2020-05-11`,
+        url: `//v7-fffblue.com/server/stats.php?task=getFacebookLikeDay&userToken=Vm5ZSmVLTjhXcWYwRzFObXlnbk5WUmlIdXF0Zk5XaGpkbXJ5ODMwc3J6Yz06OnD33aPxFDTCO6LhohyjG8o&fbId=${fbId}&from=${from_}&to=${to_}`,
         type: "GET"
     }).then(res => {
-        res = JSON.parse(res)
+        res = JSON.parse(res);
         renderChart(res)
+        $('#tableRank-fb').DataTable({
+            data: renderTable(res),
+            // drawCallback: function(settings) {},
+            columns: [{
+                    title: `<div class="text-capitalize text-center m-auto font-weight-bold font-12" style="max-width:100px;width:100px">Ngày</div>`,
+                    "data": data => `<div class="sparkline text-center m-auto" style="max-width:100px;width:100px">${data.formatDate}</div>`,
+                },
+                {
+                    title: `<div class="text-capitalize text-center m-auto font-weight-bold font-12" style="max-width:100px;width:100px">Lượt like</div>`,
+                    "data": data => `<div class="sparkline text-center m-auto" style="max-width:100px;width:100px">${data.likesday}</div>`,
+                },
+                {
+                    title: `<div class="text-capitalize text-center m-auto font-weight-bold font-12" style="max-width:100px;width:100px">Biến động</div>`,
+                    "data": data => `<div class="sparkline text-center m-auto" id="countlike" style="max-width:100px;width:100px">${data.kq} </div>`,
+                }
+            ],
+            initComplete: function(settings, json) {
+                $(`#tablefbRank td`).attr('style', 'padding:10px 18px')
+                $(`#tablefbRank_wrapper .dataTables_scrollBody`).perfectScrollbar();
+                $(`.tabletablefbRank`).removeClass('is-loading')
+            },
+            destroy: true,
+            rowId: 'trId',
+            "dom": 'ftp',
+            paging: false,
+            // scrollY: '570px', 
+            scrollX: false,
+            "ordering": false,
+
+            info: false,
+            autoWidth: false,
+            processing: true,
+            searching: false,
+            language
+        })
+
+
     })
 }
 
 
+
+
+
+
+
+// render chart 
 function renderChart(data) {
+
+
     let arrDate = [];
     let arrLikes = [];
+
     if (!data || data.data.length == 0) {
         console.log(1)
     } else {
         data.data.forEach((v, k) => {
-            let formatDate = moment(v.insertTime, "YYYY/MM/DD").format("DD/MM/YYYY")
+            let formatDate = moment(v.insertTime, "YYYY/MM/DD").format("DD/MM")
             let getLikes = v.likes
             arrDate.push(formatDate)
             arrLikes.push(getLikes)
         })
-        console.log(arrDate)
-        console.log(arrLikes)
         let myChart = echarts.init(document.getElementById(`chartLikes`), 'light')
-        console.log(1)
         let option = {
             tooltip: {
                 trigger: "axis",
@@ -120,7 +291,9 @@ function renderChart(data) {
                 },
             }, ],
             series: [{
+
                 data: arrLikes,
+                name: 'Lượt Like',
                 type: 'line',
                 smooth: true,
                 // stack: "0",
@@ -141,11 +314,12 @@ function renderChart(data) {
                 data: arrLikes
             }, ]
         };
-
-
-        new ResizeSensor($(`#chartLikes`), function() {
+        myChart.setOption(option);
+        new ResizeSensor(document.getElementById(`chartLikes`), function() {
             myChart.resize();
         });
+
+
     }
 
 }
@@ -153,5 +327,10 @@ function renderChart(data) {
 
 
 
-getDataFB();
-getLike10Days();
+
+
+$(document).ready(function() {
+    getDataFB();
+    getLike10Days();
+
+});
